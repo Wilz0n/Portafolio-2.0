3c0fa34a03a5b4c90c7387a61ea2ba330a8e85bb
Wilson <fishas02@gmail.com>
2025-06-13 17:36:17 -0500

    Se agregaron los cambios casi totales para el portafolio, falta agregar un mejor responsivo y links a los iconos

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,9 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className="absolute top-0 left-0 bottom-0 right-0 flex justify-center items-start overflow-hidden">
-      <Home_maquetado/>
-    </div>
+    <>
+    <div className="absolute top-0 left-0 bottom-0 right-0 flex justify-center items-start overflow-hidden"></div>
+    <Home_maquetado/>
+    </>
   );
 }
